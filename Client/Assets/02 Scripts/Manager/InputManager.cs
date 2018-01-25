aec12b1feb854277abc6bade026ae643f6bb6dba
DESKTOP-C8NQ13H\lawrence <nimi1220@hotmail.com>
2018-01-25 23:09:31 +0900

    Virtual Joy Pad 테스트 UI로 추가 에디터 버전 수정 InputManager 변경

--- Client/Assets/02 Scripts/Manager/InputManager.cs
+++ Client/Assets/02 Scripts/Manager/InputManager.cs
@@ -1,15 +1,20 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using CnControls;
 
 public class InputManager : MonoBehaviour {
-
     public static InputManager Instance = null;
     private bool m_bReverse;                        ///< 반전 입력 여부
+    private bool m_Enable;                          ///< 입력 가능 여부
 
     public static bool Reverse {
         get { return Instance.m_bReverse; }
         set { Instance.m_bReverse = value; }
+    }
+
+    public static bool Enable
+    {
+        get { return Instance.m_Enable; }
+        set { Instance.m_Enable = value; }
     }
 
     void Awake()
@@ -19,6 +24,7 @@ public class InputManager : MonoBehaviour {
         else
             Debug.LogError("Log has two instances");
 
+        m_Enable = true;
         m_bReverse = false;
     }
 	
@@ -27,11 +33,25 @@ public class InputManager : MonoBehaviour {
     /// </summary>
 	public static Vector3 GetMoveDir()
     {
-        Vector3 dir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+        if (Enable == false)
+            return Vector3.zero;
+
+        // Vector3 dir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+        Vector3 dir = new Vector3(CnInputManager.GetAxisRaw("Horizontal"), 0, CnInputManager.GetAxisRaw("Vertical")).normalized;
         //. 방향 반전
         if (Reverse)
             dir *= -1.0f;
 
         return dir;
+    }
+
+    /// <summary>
+    /// 버튼 입력 여부
+    /// </summary>
+    public static bool GetButtonDown(string _buttonName)
+    {
+        if(Enable)
+            return CnInputManager.GetButtonDown(_buttonName);
+        return false;
     }
 }
